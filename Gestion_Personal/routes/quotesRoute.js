ff36f1c6496a61352d27b7b16639a6d5a8832a9c
Jair <141942836+Ajendro@users.noreply.github.com>
2024-08-04 16:16:51 -0500

    cambio rutas

--- Gestion_Personal/routes/quotesRoute.js
+++ Gestion_Personal/routes/quotesRoute.js
@@ -5,8 +5,8 @@ const quoteController = require('../controllers/quotesController');
 // Define routes for managing quotes
 router.post('/quotes', quoteController.createQuote);
 router.get('/quotes', quoteController.getAllQuotes);
-router.get('/quotes/:idQuotes/:fk_idUser', quoteController.getQuoteById);
-router.put('/quotes/:idQuotes/:fk_idUser', quoteController.updateQuote);
-router.delete('/quotes/:idQuotes/:fk_idUser', quoteController.deleteQuote);
+router.get('/quotes/:idQuotes/', quoteController.getQuoteById);
+router.put('/quotes/:idQuotes/', quoteController.updateQuote);
+router.delete('/quotes/:idQuotes/', quoteController.deleteQuote);
 
 module.exports = router;
